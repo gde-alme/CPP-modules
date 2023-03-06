0f4d2cf08d72dbaccc3d0e687877552608e9552f
gasl@42 <goncalopes89@gmail.com>
2023-03-06 19:00:37 +0000

    ex02

---
+++ 00/ex02/Account.cpp
@@ -0,0 +1,26 @@
+#include "Account.hpp"
+
+Account::Account(int initial_deposit) {
+    this->_amount = initial_deposit;
+}
+
+Account::~Account() {
+}
+
+Account::getNbAccounts(void) {   
+}
+
+Account::getTotalAmount(void) {   
+}
+
+Account::getNbDeposits(void) {   
+}
+
+Account::getNbWithdrawals(void) {   
+}
+
+Account::displayAccountsInfos(void) {   
+}
+
+Account::makeDeposit(int deposit) {}
+
