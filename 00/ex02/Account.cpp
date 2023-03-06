37c29636ccab504366a0059c6dc671b6670e8bec
gasl@42 <goncalopes89@gmail.com>
2023-03-06 19:16:51 +0000

    ex02

--- 00/ex02/Account.cpp
+++ 00/ex02/Account.cpp
@@ -14,15 +14,19 @@ Account::~Account() {
 }
 
 static  int Account::getNbAccounts(void) {   
+    return (this->_nbAccounts);
 }
 
 static  int Account::getTotalAmount(void) {   
+    return (this->_totalAmount);
 }
 
 static  int Account::getNbDeposits(void) {   
+    return (this->_totalNbDeposits);
 }
 
 static  int Account::getNbWithdrawals(void) {   
+    return (this->_totalNbWithdrawals);
 }
 
 static  void Account::displayAccountsInfos(void) {   
