4ceab4d620666bf270b7548cafabcb63476d5de7
gasl@42 <goncalopes89@gmail.com>
2023-03-07 19:00:04 +0000

    newMakefile

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -3,33 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gde-alme <gde-alme@student.42lisboa.com>   +#+  +:+       +#+        */
+/*   By: gde-alme <gde-alme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 19:21:58 by gde-alme          #+#    #+#             */
-/*   Updated: 2023/01/26 19:22:00 by gde-alme         ###   ########.fr       */
+/*   Updated: 2023/03/07 17:05:44 by gde-alme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-using std::cout;
-using std::endl;
 
 int	main(int argc, char **argv) {
 	int	i = 0;
 
 	if (argc != 2) {
 		if (argc == 1)
-			cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+			std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
 	while (argv[1][i] != '\0') {
 		if (argv[1][i] > 96 && argv[1][i] < 123)
-			cout << (char)(argv[1][i] - 32);
+			std::cout << (char)(argv[1][i] - 32);
 		else
-			cout << (char)argv[1][i];
+			std::cout << (char)argv[1][i];
 		i++;
 	}
-	cout << endl;
+	std::cout << std::endl;
 	return (0);
 }
