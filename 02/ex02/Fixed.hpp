2e14e629bb097cf55520e1ee1dff31eb7b07444e
gde-alme <goncalopes98@gmail.com>
2023-03-18 14:47:47 +0000

    ex02

---
+++ 02/ex02/Fixed.hpp
@@ -0,0 +1,51 @@
+#ifndef __FIXED_HPP__
+#define __FIXED_HPP__
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+    private:
+        int     _fixedPointValue;
+        static  const   int _fractionalBits = 8;
+    public:
+        Fixed();
+        Fixed(const int num);
+        Fixed(const float num);
+        Fixed(const Fixed &oldfpn);
+        ~Fixed();
+        Fixed &operator=(const Fixed &oldfpn);
+        bool    operator>(const Fixed &opponent) const;
+        bool    operator<(const Fixed &opponent) const;
+        bool    operator>=(const Fixed &opponent) const;
+        bool    operator<=(const Fixed &opponent) const;
+        bool    operator==(const Fixed &opponent) const;
+        bool    operator!=(const Fixed &opponent) const;
+        Fixed   operator+(const Fixed &opponent) const;
+        Fixed   operator-(const Fixed &opponent) const;
+        Fixed   operator*(const Fixed &opponent) const;
+        Fixed   operator/(const Fixed &opponent) const;
+        Fixed   &operator++(void);
+        Fixed   operator++(int);
+        Fixed   &operator--(void);
+        Fixed   operator--(int);
+        int     getRawBits() const;
+        void    setRawBits(int const raw);
+        float   toFloat() const;
+        int     toInt() const;
+        /*
+        Static Function Members
+            by declaring a function member as static, you make it independent of any
+            particular object of the class. A static member function can be called 
+            even if no objects of the class exist and the static functions are accessed
+            using only the class name and the scope resolution operator ::.
+        */
+        static Fixed   &min(Fixed &a, Fixed &b);
+        static const Fixed   &min(const Fixed &a, const Fixed &b);
+        static Fixed   &max(Fixed &a, Fixed &b);
+        static const Fixed   &max(const Fixed &a, const Fixed &b);
+};
+
+std::ostream	&operator<<(std::ostream &ostreamf, Fixed const &FixedC);
+
+#endif
