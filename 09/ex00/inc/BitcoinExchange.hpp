b459d2251cd2756ca72b2f7ff11f05a3a8d23d2d
gde-alme <goncalopes98@gmail.com>
2023-04-13 13:41:59 +0100

    draw ideia-09-ex00

---
+++ 09/ex00/inc/BitcoinExchange.hpp
@@ -0,0 +1,16 @@
+#ifndef __BITCOINEXCHANGE_HPP__
+#define __BITCOINEXCHANGE_HPP__
+
+
+/*
+	*BitcoinExchange class: 
+		@parseCSV	-> populates a map<> with data from a data.csv file
+		@getValue	-> retrieves each given date to the corresponding value 
+		@parseEval	-> iterates through a txt file, calling getValue for each valid date
+*/
+class BitcoinExchange {
+	private:
+	public:
+};
+
+#endif
