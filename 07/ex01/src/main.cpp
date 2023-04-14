0f077daef6d094f81aca5742267e43c7b48e56e3
gasl@42 <goncalopes89@gmail.com>
2023-04-14 16:33:34 +0100

    small fixes

--- 07/ex01/src/main.cpp
+++ 07/ex01/src/main.cpp
@@ -1,17 +1,7 @@
 #include "../inc/iter.hpp"
-#include "../inc/data.hpp"
 
 int main() {
 	std::string	arr[] = {"ola", "adeus", "ok"};
 
 	myspace::iter(arr, 3, &(myspace::writeOut));
-
-	std::cout << std::endl << "Yes yes" << std::endl << std::endl;
-
-	Data	data_arr[2];
-
-	data_arr[0] = Data("Goncalo", 100);
-	data_arr[1] = Data("Peixinho", 42);
-
-	myspace::iter(data_arr, 2, &(myspace::writeOut));
 }
